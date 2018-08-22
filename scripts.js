07ffecc1aefde16fe22583aeb2a0b3e312614e6b
Tory Dannenberg <rfdberg@gmail.com>
2018-08-22 10:46:15 -0600

    Add header and initial tab structure

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+var accordions = document.getElementsByClassName('accordion-items');
+
+console.log(accordions[0])
+const handleClick = (event) => {
+  event.preventDefault();
+  console.log(event.target);
+};
+
+let i;
+
+for (i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener('click', handleClick)
+}
+
+// $(accordions).each(accordion => {
+//   $(accordion).on('click', handleClick)
+// })
